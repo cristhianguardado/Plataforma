958027166d8dc6337c438133f6607025da09fdc4
cristhianguardado <estrada.cristhian9@gmail.com>
2014-10-19 23:07:09 -0500

    agregado nuevo folder

---
+++ components/paginasestaticas/index.js
@@ -0,0 +1,12 @@
+var include = require("includemvc");
+var include = require("includemvc");
+var app = include.app();
+var controller = require("./controllers");
+
+app.get("/", controller.frontpage);
+app.get("/403", controller.error403);
+app.get("/404", controller.error404);
+app.get("/error", controller.error);
+
+//prueba
+app.get("/test", controller.test);
